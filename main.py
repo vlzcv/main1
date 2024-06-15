da8f62bb6b5b52a774d0fbcb370db1c9d7be0c65
vlzcv <vlada.zaytseva.2009@mail.ru>
2024-06-15 04:43:16 +0300

    добавление файлов

---
+++ main.py
@@ -0,0 +1,29 @@
+a = "Dog"
+lenght = len(a)
+print(lenght)
+
+first = 5
+second = 4
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+
+numbers = (2, 6, 4)
+sum_of_numbers = sum(numbers)
+count_of_numbers = len(numbers)
+average = sum_of_numbers / count_of_numbers
+print(average)
+
+first_string = ("Понедельник")
+second_string = ("Вторник")
+print(first_string, second_string, sep= ', ')
+
+a = 2
+b = 3
+c = 5
+f = (a*b) + (a*c)
+d = f**3
+g = d/2
+print(g)
+
